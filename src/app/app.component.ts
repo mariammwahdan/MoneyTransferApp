8c2506abe1f7359ca85bb56df5d1b92f268fd1ee
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-09-01 14:24:48 +0300

    Main Footer Component is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainFooterComponent } from './components/main-footer/main-footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainFooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'MoneyTransferApp';
