88b6fe014971853e759b452c38813dcef7a65934
Rana-ElFetyany <ranafet777@gmail.com>
2024-09-04 01:50:46 +0300

    add shared btn component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainFooterComponent } from './components/main-footer/main-footer.component';
 import { MobileAppSectionComponent } from './components/mobile-app-section/mobile-app-section.component';
+import { BtnComponent } from "./shared/btn/btn.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainFooterComponent, MobileAppSectionComponent],
+  imports: [RouterOutlet, MainFooterComponent, MobileAppSectionComponent, BtnComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/shared/btn/btn.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn.component.html',
+  styleUrl: './btn.component.scss',
+})
+export class BtnComponent {
+  @Input() label = '';
+  @Input() background = 'dark-color';
+  @Input() width = '';
+}
