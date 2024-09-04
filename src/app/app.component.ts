71efe1db399b2351bc1b896c6e3664ef2dbb23ee
Rana-ElFetyany <ranafet777@gmail.com>
2024-09-04 04:17:16 +0300

    Add 404 Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MainFooterComponent } from './components/main-footer/main-footer.component';
 import { MobileAppSectionComponent } from './components/mobile-app-section/mobile-app-section.component';
 import { BtnComponent } from "./shared/btn/btn.component";
+import { Error404Component } from "./components/error-404/error-404.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainFooterComponent, MobileAppSectionComponent, BtnComponent],
+  imports: [RouterOutlet, MainFooterComponent, MobileAppSectionComponent, BtnComponent, Error404Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
