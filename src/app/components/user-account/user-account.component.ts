563d213e441936d2a1b628aff7da880a945a0ddf
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-09-05 17:59:01 +0300

    unfinished my-acc section and will be entirely modified

--- src/app/components/user-account/user-account.component.ts
+++ src/app/components/user-account/user-account.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { MainFooterComponent } from '../main-footer/main-footer.component';
 import { MobileAppSectionComponent } from '../mobile-app-section/mobile-app-section.component';
+import { MyProfileComponent } from '../my-profile/my-profile.component';
+import { PaymentHistoryComponent } from '../payment-history/payment-history.component';
 
 @Component({
   selector: 'app-user-account',
   standalone: true,
-  imports: [MainFooterComponent, MobileAppSectionComponent],
+  imports: [
+    MainFooterComponent,
+    MobileAppSectionComponent,
+    MyProfileComponent,
+    PaymentHistoryComponent,
+  ],
   templateUrl: './user-account.component.html',
   styleUrl: './user-account.component.scss',
 })
