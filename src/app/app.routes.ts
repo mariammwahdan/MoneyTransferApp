7bb9e179d5bc4469cde6f78a28bfde6af56a1e3e
Aml Ali <amalshalata64@gmail.com>
2024-09-05 15:50:31 +0300

    Navbar basic routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { PaymentComponent } from './components/payment/payment.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { UserAccountComponent } from './components/user-account/user-account.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -11,7 +15,16 @@ export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'transferMoney', component: MoneyTransferComponent },
-    { path: 'myAccount', component: UserAccountComponent, title: "My Account" },
+    {
+        path: 'myAccount', component: UserAccountComponent, title: "My Account",
+        children: [
+            { path: '', redirectTo: 'profile', pathMatch: 'full' },
+            { path: 'profile', component: UserProfileComponent },
+            { path: 'payment', component: PaymentComponent },
+            { path: 'setting', component: SettingComponent },
+            { path: 'changePassword', component: ChangePasswordComponent },
+        ]
+    },
     { path: 'help', component: HelpComponent, title: 'Help' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
