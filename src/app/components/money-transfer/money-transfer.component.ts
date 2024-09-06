3c82608f7a15df5449e3de0b759e57a7151a2a36
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-09-06 13:22:23 +0300

    handled bug where footer doesnt show in logged in state

--- src/app/components/money-transfer/money-transfer.component.ts
+++ src/app/components/money-transfer/money-transfer.component.ts
@@ -3,17 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { MainFooterComponent } from '../main-footer/main-footer.component';
 import { MobileAppSectionComponent } from '../mobile-app-section/mobile-app-section.component';
 import { AuthService } from '../../core/services/auth.service';
-import { TopSectionComponent } from "../../shared/top-section/top-section.component";
+import { TopSectionComponent } from '../../shared/top-section/top-section.component';
 
 @Component({
   selector: 'app-money-transfer',
   standalone: true,
-  imports: [MobileAppSectionComponent, MainFooterComponent, TopSectionComponent],
+  imports: [
+    MobileAppSectionComponent,
+    MainFooterComponent,
+    TopSectionComponent,
+  ],
   templateUrl: './money-transfer.component.html',
   styleUrl: './money-transfer.component.scss',
 })
 export class MoneyTransferComponent implements OnInit {
-  constructor(public _Nav: AuthService) { }
+  constructor(public _Nav: AuthService) {}
   ngOnInit() {
     this._Nav.show();
   }
