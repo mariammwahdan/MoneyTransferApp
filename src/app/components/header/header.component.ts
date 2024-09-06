3861eb843a7d41b52b58c575ba110ce96389dafa
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-09-06 03:47:20 +0300

    modified style in about section component and modified routing of transfer in auth-navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 import { BtnComponent } from '../../shared/btn/btn.component';
 import { NgClass } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [BtnComponent, NgClass],
-
+  imports: [BtnComponent, NgClass, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/components/user-account/user-account.component.ts
+++ src/app/components/user-account/user-account.component.ts
@@ -3,29 +3,19 @@ import { MainFooterComponent } from '../main-footer/main-footer.component';
 import { MobileAppSectionComponent } from '../mobile-app-section/mobile-app-section.component';
 import { AuthService } from '../../core/services/auth.service';
 import { TopSectionComponent } from '../../shared/top-section/top-section.component';
-import { PaymentComponent } from '../payment/payment.component';
-import { UserProfileComponent } from '../user-profile/user-profile.component';
-import { SettingComponent } from '../setting/setting.component';
-import { ChangePasswordComponent } from '../change-password/change-password.component';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-account',
   standalone: true,
-
   imports: [
     RouterLinkActive,
-    RouterOutlet,
     RouterLink,
+    RouterOutlet,
     MainFooterComponent,
     MobileAppSectionComponent,
-    UserProfileComponent,
-    PaymentComponent,
     TopSectionComponent,
-    SettingComponent,
-    ChangePasswordComponent,
   ],
-
   templateUrl: './user-account.component.html',
   styleUrl: './user-account.component.scss',
 })
