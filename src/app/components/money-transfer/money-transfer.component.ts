0e6b6c5acf70f8faf909bac0fb05ad4381d66e8b
Mariam Wahdan <mariamwahdan32@gmail.com>
2024-09-07 14:15:31 +0300

    transfer-confirmaation section

--- src/app/components/money-transfer/money-transfer.component.ts
+++ src/app/components/money-transfer/money-transfer.component.ts
@@ -4,6 +4,7 @@ import { MainFooterComponent } from '../main-footer/main-footer.component';
 import { MobileAppSectionComponent } from '../mobile-app-section/mobile-app-section.component';
 import { AuthService } from '../../core/services/auth.service';
 import { TopSectionComponent } from '../../shared/top-section/top-section.component';
+import { TransferConfirmationComponent } from '../transfer-confirmation/transfer-confirmation.component';
 
 @Component({
   selector: 'app-money-transfer',
@@ -12,6 +13,7 @@ import { TopSectionComponent } from '../../shared/top-section/top-section.compon
     MobileAppSectionComponent,
     MainFooterComponent,
     TopSectionComponent,
+    TransferConfirmationComponent,
   ],
   templateUrl: './money-transfer.component.html',
   styleUrl: './money-transfer.component.scss',
